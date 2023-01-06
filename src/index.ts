ba6a2dca3630b5eaa939737021ff4e72b907dbbd
João Costa <costav1402@gmail.com>
2023-01-05 22:20:17 -0300

    feat: connect with mongo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import 'graphql-import-node'
-import { getGraphQLParameters, processRequest, Request, sendResult} from 'graphql-helix'
+import { getGraphQLParameters, processRequest, renderGraphiQL, Request, sendResult, shouldRenderGraphiQL} from 'graphql-helix'
 import Koa from 'koa'
 import Router from "@koa/router"
 import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors';
 import { schema } from './schema'
+import mongoose from 'mongoose'
+import * as dotenv from "dotenv";
+dotenv.config();
 async function main () {
+    const url = process.env.MONGO_URL as string
     const router = new Router()
     const server = new Koa()
-    
+    await mongoose.connect(url)
+    console.log('Connected to MongoDb')
     router.register('/graphql', ['get', 'post'], async (ctx, next) => {
         const request:Request = {
             headers: ctx.request.headers,
@@ -16,6 +21,15 @@ async function main () {
             query: ctx.request.query,
             body:ctx.request.body
         }
+        if (shouldRenderGraphiQL(request)) {
+            ctx.request.header = {"Content-Type": "text/html"}
+            ctx.body = renderGraphiQL({
+                endpoint: "/graphql",
+              }
+            );
+    
+            return;
+          }
         const { operationName, query, variables } = getGraphQLParameters(request)
 
         const result = await processRequest({
@@ -23,7 +37,7 @@ async function main () {
             schema,
             operationName,
             query,
-            variables
+            variables,
         })
         sendResult(result, ctx.res)
     })
