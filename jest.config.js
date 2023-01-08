1248b1da7bd128d2c5e23af10ec99ce5656df832
João Costa <costav1402@gmail.com>
2023-01-08 14:21:09 -0300

    chore: add babel for tests settings

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.{ts,tsx}'
-    ],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'node',
-    transform: {
-      '.+\\.ts$': 'ts-jest'
-    }
+  collectCoverage: true,
+  // on node 14.x coverage provider v8 offers good speed and more or less good report
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  moduleNameMapper: {
+  },
+  // Add more setup options before each test is run
+  testPathIgnorePatterns: ['<rootDir>/node_modules/',],
+  testEnvironment: 'node',
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
 }
