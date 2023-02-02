97289f99751baac5347d905b89cc9dd3737c3358
João Costa <costav1402@gmail.com>
2023-02-02 20:05:01 -0300

    test: add login mutation schema

--- src/test/user.spec.test.ts
+++ src/test/user.spec.test.ts
@@ -47,7 +47,35 @@ describe('CRUD User', () => {
     expect(authPayload?.user).toHaveProperty('email')
     expect(authPayload?.user).toHaveProperty('name')
     })
-    it.todo('should be login')
+    it('should be login', async () => {
+        const user = {
+            name:'Jhon Doe',
+            email:'jhondoe@email.com',
+            password:'123'
+        }
+        const source = `
+            mutation{
+                login(email:"${user.email}", password:"${user.password}"){
+                token
+                user{
+                name
+                email
+                }
+            }
+        }
+        `
+    const { data } = await graphql({ schema, source })
+    const authPayload = data?.login as unknown as AuthPayload
+    expect(authPayload).not.toBeNull()
+    expect(authPayload).not.toBeUndefined()
+    expect(authPayload).toHaveProperty('token')
+    expect(authPayload).toHaveProperty('user')
+    expect(authPayload?.user.email).toBe(user.email)
+    expect(authPayload?.user.name).toBe(user.name)
+    expect(authPayload?.user).toHaveProperty('name')
+    expect(authPayload?.user).toHaveProperty('email')
+    expect(authPayload?.user).toHaveProperty('name')
+    })
     it.todo('should be show all users')
     it.todo('should be show single user')
     it.todo('should be update user')
